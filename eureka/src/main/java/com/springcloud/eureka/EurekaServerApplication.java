648f1b860d560a491192925e9a4fb7de88b00679
nojudgeme <15172426627@163.com>
2018-07-24 16:02:40 +0800

    springcloud demo

--- eureka/src/main/java/com/springcloud/eureka/EurekaServerApplication.java
+++ eureka/src/main/java/com/springcloud/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.springcloud.demo;
+package com.springcloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka/src/test/java/com/springcloud/eureka/DemoApplicationTests.java
+++ eureka/src/test/java/com/springcloud/eureka/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springcloud.demo;
+package com.springcloud.eureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
